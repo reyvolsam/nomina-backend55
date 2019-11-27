39e0ce7d2e05c79f33839a2f6dfcb74d74bb5780
Samuel Regino Placido <samuel.regino@gnp.com.mx>
2019-11-27 11:30:45 -0600

    Se termino la edicion del modulo de nomina

--- app/Http/Controllers/NominaController.php
+++ app/Http/Controllers/NominaController.php
@@ -35,10 +35,16 @@ class NominaController extends Controller
 
                 foreach ($list as $kl => $vl) {
                     if(count($vl->nomina_excel) > 0){
-                        foreach ($vl->nomina_excel as $ne => $vne) $vne->file_url = asset('nomina_excel/'.$vne->file_url);    
+                        foreach ($vl->nomina_excel as $ne => $vne){
+                            $vne->file_url = asset('nomina_excel/'.$vne->file_url);
+                            $vne->deleted = false;
+                        }
                     }
                     if(count($vl->nomina_dispersion) > 0){
-                        foreach ($vl->nomina_dispersion as $ne => $vne) $vne->file_url = asset('dispersion_files/'.$vne->file_url);    
+                        foreach ($vl->nomina_dispersion as $ne => $vne){
+                            $vne->file_url = asset('dispersion_files/'.$vne->file_url);
+                            $vne->deleted = false;
+                        }
                     }
                 }
 
@@ -75,14 +81,66 @@ class NominaController extends Controller
             $this->res['data'] = $_FILES;
             $this->res['input'] = $_REQUEST;
 
-            if(count($_FILES) > 0 ){
+            $id   = $_REQUEST['id'];
 
+            if($id == 'null'){
                 $nomina = new Nomina();
-                $nomina->date   = $_REQUEST['nomina_date'];
-                $nomina->period = $_REQUEST['nomina_period'];
+                $nomina->date   = $_REQUEST['date'];
+                $nomina->period = $_REQUEST['period'];
                 $nomina->obra   = $_REQUEST['obra'];
                 $nomina->save();
 
+                $this->uploadNominaFiles($nomina->id);
+            } else {
+                $nomina = Nomina::find($id);
+                if($nomina){
+                    $nomina->date   = $_REQUEST['date'];
+                    $nomina->period = $_REQUEST['period'];
+                    $nomina->obra   = $_REQUEST['obra'];
+                    $nomina->save();
+                    
+                    $this->uploadNominaFiles($nomina->id);
+                    
+                    if(isset($_REQUEST['excel_edit'])){
+                        $excel_edit = $_REQUEST['excel_edit'];
+                        if(count($excel_edit) > 0){
+                            foreach ($excel_edit as $kee => $vee) {
+                                if($vee['deleted'] == 'true'){
+                                    $deleteN = NominaExcel::find($vee['id']);
+                                    if($deleteN) $deleteN->delete();
+                                }
+                            }
+                        }
+                    }
+
+                    if(isset($_REQUEST['dispersion_edit'])){
+                        $dispersion_edit = $_REQUEST['dispersion_edit'];
+                        if(count($dispersion_edit) > 0){
+                            foreach ($dispersion_edit as $kde => $vde) {
+                                if($vde['deleted'] == 'true'){
+                                    $deleteD = NominaDispersion::find($vde['id']);
+                                    if($deleteD) $deleteD->delete();
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    $this->res['message'] = 'Este registro de nomina no existe.';        
+                }
+            }
+            $this->status_code = 200;
+        } catch(\Exception $e) {
+            $this->res['message'] = 'Error en la Base de Datos.'.$e;
+            $this->status_code = 500;
+        }
+        return response()->json($this->res, $this->status_code);
+    }
+
+    private function uploadNominaFiles($nomina_id)
+    {
+        if(count($_FILES) > 0 ){
+
+            if(isset($_FILES['excel_files'])){
                 if(count($_FILES['excel_files']['name'])){
                     foreach ($_FILES['excel_files']['name'] as $kef => $vef) {
                         $porciones = explode(".", $vef);
@@ -95,7 +153,7 @@ class NominaController extends Controller
                         $final_image_name = $rand."_".time().".".$ext;
                         if(move_uploaded_file($_FILES['excel_files']['tmp_name'][$kef], 'nomina_excel/'.basename($final_image_name))){
                             $nomina_excel = new NominaExcel();
-                            $nomina_excel->nomina_id = $nomina->id;
+                            $nomina_excel->nomina_id = $nomina_id;
                             $nomina_excel->file_url = $final_image_name;
                             $nomina_excel->name = $vef;
                             $nomina_excel->save();
@@ -105,7 +163,9 @@ class NominaController extends Controller
                         }
                     }
                 }
+            }
 
+            if(isset($_FILES['dispersion_files'])){
                 if(count($_FILES['dispersion_files']['name'])){
                     foreach ($_FILES['dispersion_files']['name'] as $kdf => $vdf) {
                         $porciones = explode(".", $vdf);
@@ -118,7 +178,7 @@ class NominaController extends Controller
                         $final_image_name = $rand."_".time().".".$ext;
                         if(move_uploaded_file($_FILES['dispersion_files']['tmp_name'][$kdf], 'dispersion_files/'.basename($final_image_name))){
                             $nomina_dispersion = new NominaDispersion();
-                            $nomina_dispersion->nomina_id = $nomina->id;
+                            $nomina_dispersion->nomina_id = $nomina_id;
                             $nomina_dispersion->file_url = $final_image_name;
                             $nomina_dispersion->name = $vdf;
                             $nomina_dispersion->save();
@@ -128,14 +188,10 @@ class NominaController extends Controller
                         }
                     }
                 }
-                $this->status_code = 200;
             }
-        } catch(\Exception $e) {
-            $this->res['message'] = 'Error en la Base de Datos.'.$e;
-            $this->status_code = 500;
+
         }
-        return response()->json($this->res, $this->status_code);
-    }
+    }//
 
     /**
      * Display the specified resource.
@@ -166,9 +222,24 @@ class NominaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update($id)
     {
-        //
+        try {
+            $this->res['data'] = $_FILES;
+            $this->res['input'] = $_REQUEST;
+            $this->res['OK'] = 'si es tes';
+
+            /*if(count($_FILES) > 0 ){
+
+
+                
+            }*/
+            $this->status_code = 200;
+        } catch(\Exception $e) {
+            $this->res['message'] = 'Error en la Base de Datos.'.$e;
+            $this->status_code = 500;
+        }
+        return response()->json($this->res, $this->status_code);
     }
 
     /**
@@ -179,6 +250,41 @@ class NominaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try{
+            if(is_numeric($id)){
+                $nomina = Nomina::find($id);
+                if($nomina){
+                    $nomina->delete();
+                    $nomina_excel = NominaExcel::where('nomina_id', $id)->get();
+                    if(count($nomina_excel) > 0){
+                        foreach ($nomina_excel as $kne => $vne) {
+                            $nw = NominaExcel::find($vne->id);
+                            if($nw) $nw->delete();    
+                        }
+                    }
+
+                    $nomina_dispersion = NominaDispersion::where('nomina_id', $id)->get();
+                    if(count($nomina_dispersion) > 0){
+                        foreach ($nomina_dispersion as $knd => $vnd) {
+                            $nd = NominaDispersion::find($vnd->id);
+                            if($nd) $nd->delete();    
+                        }
+                    }
+
+                    $this->res['message'] = 'Nomina eliminada correctamente.';
+                    $this->status_code = 200;
+                } else {
+                    $this->res['message'] = 'El registro de Nomina no existe.';
+                    $this->status_code = 422;
+                }
+            } else {
+                $this->res['message'] = 'ID incorrecto.';
+                $this->status_code = 422;
+            }
+        } catch(\Exception $e) {
+            $this->res['message'] = 'Error en el sistema.'.$e;
+            $this->status_code = 422;
+        }
+        return response()->json($this->res, $this->status_code);
     }
 }
